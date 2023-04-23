0ae143cf81f1ebadec475e6ff646d7ffd3c7f163
YayaTheBehemoth <allanermansur@gmail.com>
2023-04-23 14:26:56 +0200

    stuff

--- Controllers/bookingController.cs
+++ Controllers/bookingController.cs
@@ -14,10 +14,17 @@ public class bookingController : ControllerBase
 
  private readonly string _docPath;
 
+ private readonly string _hostingName; 
+
     public bookingController(ILogger<bookingController> logger, IConfiguration config)
     {
         _docPath = config["DocPath"];
         _logger = logger;
+        _hostingName = config["hostingname"];
+        var hostName = System.Net.Dns.GetHostName();
+var ips = System.Net.Dns.GetHostAddresses(hostName);
+var _ipaddr = ips.First().MapToIPv4().ToString();
+_logger.LogInformation(1, $"Taxabooking responding from {_ipaddr}");
     }
 
     //Note: If I had to make more methods I most likely would have made a "service" component to handle rabbitmq related things 
@@ -27,19 +34,22 @@ public class bookingController : ControllerBase
     public void Post([FromBody]BookingDTO booking) 
     {
         //send info to logger
-        _logger.LogInformation("Method 'Post' from service TaxaBooking called at {DT}",  
+        _logger.LogInformation("Method 'PostBooking' from service TaxaBooking called at {DT}",  
         DateTime.UtcNow.ToLongTimeString()); 
+         
         //convert from BookingDTO to PlanDTO 
         PlanDTO newPlan = new PlanDTO();
         newPlan.CustomerName = booking.CustomerName;
         newPlan.start = booking.start;
         newPlan.slut = booking.slut;
         newPlan.id = booking.id;
+        
         //Create connection to rabbitmq server
-        var factory = new ConnectionFactory { HostName = "localhost" };
+        var factory = new ConnectionFactory { HostName = "_hostname"  };
         using var connection = factory.CreateConnection();
         //use connection to make channel
         using var channel = connection.CreateModel();
+        _logger.LogInformation($"Making queue using {factory.HostName}");
         //use channel to declare queue
         channel.QueueDeclare(queue: "Planning Service",
                      durable: false,
@@ -53,21 +63,23 @@ public class bookingController : ControllerBase
                      routingKey: "Planning Service",
                      basicProperties: null,
                      body: body);
+                     
     
     }
     //method to get the plan.csv
     [HttpGet(Name = "getPlanFile")]
      public FileStreamResult GetFile()
         {
-         _logger.LogInformation("Method 'Get' from service TaxaBooking called at {DT}",  
+         _logger.LogInformation("Method 'GetPlanFile' from service TaxaBooking called at {DT}",  
         DateTime.UtcNow.ToLongTimeString()); 
             //define path to find file
             var physicalPath = _docPath;
+            _logger.LogInformation($"fetching files from {_docPath}");
             //read the file
-            var pdfBytes = System.IO.File.ReadAllBytes(physicalPath);
+            var pdfBytes = System.IO.File.ReadAllBytes(Path.Combine(_docPath,"Plan.csv"));
             //stream result so it can be returned
             var ms = new MemoryStream(pdfBytes);
-            return new FileStreamResult(ms,"csv");
+            return File(ms, "text/csv");
         }
 }
     
